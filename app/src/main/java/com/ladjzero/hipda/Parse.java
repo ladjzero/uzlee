ce82094686f54e1de09b4032ef11e974db07c4d2
chenzhuo <ladjzero@gmail.com>
2017-04-24 11:32:31 +0800

    add Reponse and Parse interface

---
+++ app/src/main/java/com/ladjzero/hipda/Parse.java
@@ -0,0 +1,8 @@
+package com.ladjzero.hipda;
+
+/**
+ * Created by chenzhuo on 2017/4/23.
+ */
+public interface Parse {
+	Response parse(String html);
+}
---
+++ app/src/main/java/com/ladjzero/hipda/Response.java
@@ -0,0 +1,85 @@
+package com.ladjzero.hipda;
+
+import java.io.Serializable;
+
+/**
+ * Created by chenzhuo on 2017/4/23.
+ */
+public class Response implements Serializable {
+	private boolean success;
+	private Meta meta;
+
+	public Object getData() {
+		return data;
+	}
+
+	public void setData(Object data) {
+		this.data = data;
+	}
+
+	public Meta getMeta() {
+		return meta;
+	}
+
+	public void setMeta(Meta meta) {
+		this.meta = meta;
+	}
+
+	public boolean isSuccess() {
+		return success;
+	}
+
+	public void setSuccess(boolean success) {
+		this.success = success;
+	}
+
+	private Object data;
+
+	public static class Meta {
+		public String getCode() {
+			return code;
+		}
+
+		public void setCode(String code) {
+			this.code = code;
+		}
+
+		public String getFormhash() {
+			return formhash;
+		}
+
+		public void setFormhash(String formhash) {
+			this.formhash = formhash;
+		}
+
+		public String getHash() {
+			return hash;
+		}
+
+		public void setHash(String hash) {
+			this.hash = hash;
+		}
+
+		public int getUnread() {
+			return unread;
+		}
+
+		public void setUnread(int unread) {
+			this.unread = unread;
+		}
+
+		public User getUser() {
+			return user;
+		}
+
+		public void setUser(User user) {
+			this.user = user;
+		}
+
+		private User user;
+		private String formhash;
+		private String hash;
+		private String code = Parser.CODE_GBK;
+		private int unread;
+	}
+}
